4c8706471095b50fe65fbf9bdc01722f07fb1f25
mrwaks <ouaknine.rudy@gmail.com>
2021-12-18 19:48:13 +0100

    cli-rmtrash V1.0.0

---
+++ src/v_linux/rmTrash_vlinux.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+ITEMS=$@
+TRASH_CONTENT=`cd ~/.RMTRASH && ls -A`
+OS=`uname`
+
+RANDOM=$((1 + RANDOM % 50))
+
+DAY=`date +%d`
+MONTH=`date +%m`
+YEAR=`date +%y`
+DATE="$DAY-$MONTH-$YEAR"
+
+HOUR=`date +%H`
+MINUTE=`date +%M`
+SECOND=`date +%S`
+TIME="$HOUR:$MINUTE:$SECOND"
+
+BIS="$DATE-$TIME-id$RANDOM"
+
+# HELPER:
+
+helper() {
+    echo -e "Help:\n\nCommands:\n\n  rmtrash [...files/directory]    To place one or more items in the rmtrash bin\n\n  rmtrash ls                      To view the contents in the rmtrash bin"
+}
+
+# VERSION:
+
+version() {
+    echo "1.0.0"
+}
+
+# PROGRAMS STEPS:
+
+checkOS() {
+    [ $OS = "Linux" ] && return 0 || echo "Os not compatible" && return 1
+}
+
+checkItemsExists() {
+    for ITEM in $ITEMS; do [ -e $ITEM ] && ITEMS_TO_DELETE+="$ITEM " || echo "rmtrash: $ITEM: no such file or directory"; done
+}
+
+checkItemsExistInTrash() {
+    for ITEM in $ITEMS_TO_DELETE; do [ -e ~/.RMTRASH/$ITEM ] && mv "$ITEM" "$ITEM-$BIS" && VALID_ITEMS+="$ITEM-$BIS " || VALID_ITEMS+="$ITEM "; done
+}
+
+rmTrash() {
+    for ITEM in $VALID_ITEMS; do mv "$ITEM" ~/.RMTRASH; done
+}
+
+rmTrashList() {
+    if [ ${#TRASH_CONTENT} -eq 0 ]; then echo "rmtrash empty"; else echo -e "\x1b[4mrmtrash content:\x1b[0m\n$TRASH_CONTENT"; fi
+}
+
+
+if [ $# -eq 0 ]
+then
+    helper
+elif [ "$1" = "ls" ]
+then
+    rmTrashList
+elif [ "$1" = "--help" ] || [ "$1" = "-h" ]
+then
+    helper
+elif [ "$1" = "--version" ] || [ "$1" = "-v" ]
+then
+    version
+else
+    checkOS &&
+    checkItemsExists
+    checkItemsExistInTrash
+    rmTrash
+fi
---
+++ src/v_macos/emptyRmTrash_vmacos.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+OS=`uname`
+
+TRASH_CONTENT=`cd ~/.RMTRASH && ls -A`
+
+shopt -s nocasematch;
+
+# HELPER:
+
+helper() {
+    echo -e "Help:\n\nCommands:\n\n  emtrash        To empty the rmtrash with confirmation\n\n  emtrash -y     To empty the rmtrash trash without confirmation"
+}
+
+# VERSION:
+
+version() {
+    echo "1.0.0"
+}
+
+# PROGRAMS STEPS:
+
+checkOS() {
+    [ $OS = "Darwin" ] && return 0 || echo "Os not compatible" && return 1
+}
+
+checkContentTrash() {
+    [ ${#TRASH_CONTENT} -eq 0 ] && echo "rmtrash empty" && exit 1
+}
+
+question() {
+    while [ -z $ANSWER ]; do read -p "Are you sure to empty the rmtrash ? (Y/n) " ANSWER; if [[ "y" =~ $ANSWER ]]; then return 0; elif [[ "n" =~ $ANSWER ]]; then return 1; else return 2; fi; done
+}
+
+emptyTrash() {
+    for ITEM in $TRASH_CONTENT; do cd ~/.RMTRASH && rm -r "$ITEM"; done
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]
+then
+    helper
+elif [ "$1" = "-v" ] || [ "$1" = "--version" ]
+then
+    version
+elif [ "$1" = "-y" ]
+then
+    emptyTrash
+elif [ $# -eq 0 ]
+then
+    checkOS &&
+    checkContentTrash ||
+    question
+    case $? in
+        0)
+            emptyTrash && echo "rmtrash successfully emptied"
+            ;;
+        1)
+            echo "Cancelation"
+            ;;
+        *)
+            echo "Invalid answer"
+            ;;
+    esac
+fi
